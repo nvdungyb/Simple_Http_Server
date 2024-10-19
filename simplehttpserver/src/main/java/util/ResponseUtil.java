5b22f91eab5a7f731f522e69bb379ad8ccc169c5
NvDzung <dung3032003md@gmail.com>
2024-10-19 14:56:49 +0700

    Http response

---
+++ simplehttpserver/src/main/java/util/ResponseUtil.java
@@ -0,0 +1,40 @@
+package util;
+
+import config.TargetResources;
+import http1.HttpStatusCode;
+
+import java.io.*;
+
+public class ResponseUtil {
+    public static ObjectResponse readFile(String requestTarget, TargetResources targetResources) {
+        String filePath;
+        String fileExtension;
+        if (targetResources.getResources().containsKey(requestTarget)) {
+            filePath = targetResources.getResources().get(requestTarget);
+            fileExtension = getFileExtension(filePath);
+        } else {
+            throw new RuntimeException(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST.MESSAGE);
+        }
+
+        try {
+            BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(filePath));
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            int bytesRead;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byteArrayOutputStream.write(buffer, 0, bytesRead);
+            }
+
+            return new ObjectResponse(byteArrayOutputStream, fileExtension);
+        } catch (FileNotFoundException exception) {
+            throw new RuntimeException(exception.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getFileExtension(String target) {
+        int lastIndexOfDot = target.lastIndexOf('.');
+        return target.substring(lastIndexOfDot + 1);
+    }
+}
